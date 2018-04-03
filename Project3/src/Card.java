f7de0547dbfcf28204e19d68ea0535a7af02ab0b
Paul Gotcher <33100956+Auditre@users.noreply.github.com>
2018-04-03 15:48:30 -0700

    Working board, no game
    The board initializes properly, but with zero game functionality.

---
+++ Project3/src/Card.java
@@ -0,0 +1,125 @@
+import java.awt.Image;
+
+/*****************************************************************
+   Class Card, the derived class each card is one object of type Card
+   May be placed in a file named Card.java
+******************************************************************/
+
+class Card extends Link {
+  private Image cardimage;
+  private int rank;
+  private int cardnum;
+  private boolean isFaceDown = false;
+  
+
+  public Card (int cardnumber) {
+	this.cardnum = cardnumber;
+    cardimage = Project3.load_picture("images/gbCard" + cardnum + ".gif");
+    // code ASSUMES there is an images sub-dir in your project folder
+    
+    //ACES
+    if(cardnum == 0 ||cardnum == 13 ||cardnum == 26 ||cardnum == 39) {
+    	this.rank = 1;
+    }
+    //TWOS
+    else if(cardnum == 1 ||cardnum == 14 ||cardnum == 27 ||cardnum == 40) {
+    	this.rank = 2;
+    }
+    //THREES
+    else if(cardnum == 2 ||cardnum == 15 ||cardnum == 28 ||cardnum == 41) {
+    	this.rank = 3;
+    }
+    //FOURS
+    else if(cardnum == 3 ||cardnum == 16 ||cardnum == 29 ||cardnum == 42) {
+    	this.rank = 4;
+    }
+    //FIVES
+    else if(cardnum == 4 ||cardnum == 17 ||cardnum == 30 ||cardnum == 43) {
+    	this.rank = 5;
+    }
+    //SIXES
+    else if(cardnum == 5 ||cardnum == 18 ||cardnum == 31 ||cardnum == 44) {
+    	this.rank = 6;
+    }
+    //SEVENS
+    else if(cardnum == 6 ||cardnum == 19 ||cardnum == 32 ||cardnum == 45) {
+    	this.rank = 7;
+    }
+    //EIGHTS
+    else if(cardnum == 7 ||cardnum == 20 ||cardnum == 33 ||cardnum == 46) {
+    	this.rank = 8;
+    }
+    //NINES
+    else if(cardnum == 8 ||cardnum == 21 ||cardnum == 34 ||cardnum == 47) {
+    	this.rank = 9;
+    }
+    //TENS
+    else if(cardnum == 9 ||cardnum == 22 ||cardnum == 35 ||cardnum == 48) {
+    	this.rank = 10;
+    }
+    //JACKS
+    else if(cardnum == 10 ||cardnum == 23 ||cardnum == 36 ||cardnum == 49) {
+    	this.rank = 11;
+    }
+    //QUEENS
+    else if(cardnum == 11 ||cardnum == 24 ||cardnum == 37 ||cardnum == 50) {
+    	this.rank = 12;
+    }
+    //KINGS
+    else if(cardnum == 12 ||cardnum == 25 ||cardnum == 38 ||cardnum == 51) {
+    	this.rank = 13;
+    }
+    else {
+    	System.out.println("No card rank found");
+    	System.exit(-1);
+    }
+    
+    
+    
+    if (cardimage == null) {
+      System.out.println("Error - image failed to load: images/gbCard" + cardnum + ".gif");
+      System.exit(-1);
+    }
+  }
+  public Card getNextCard() {
+    return (Card)next;
+  }
+  
+  public void setRank(int r) {
+	  this.rank = r;
+	  
+  }
+  
+  public int getRank() {
+	  return rank;
+  }
+  
+// This method should never have to be used
+//  public void setCardnum(int cardnumber) {
+//	  this.cardnum = cardnumber;
+//  }
+  
+  public int getCardnum() {
+	  return this.cardnum;
+  }
+  
+  public Image getCardImage() {
+    return cardimage;
+  }
+  
+  public void setCardImage(Image im) {
+	  this.cardimage = im;
+	  
+  }
+  
+  public boolean getIsFaceDown() {
+	  return this.isFaceDown;
+  }
+  
+  public void setIsFaceDown(boolean face) {
+	  this.isFaceDown = face;
+  }
+  
+  
+  
+} 
---
+++ Project3/src/Project3.java
@@ -0,0 +1,270 @@
+/***************************************************************
+  Project Number 3 - Comp Sci 182 - Data Structures (w/ Swing)
+  Start Code - Build your program starting with this code
+               Card Game
+  Copyright 2005-2016 Christopher C. Ferguson
+  This code may only be used with the permission of Christopher C. Ferguson
+***************************************************************/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Project3 extends JFrame implements ActionListener {
+	
+	
+	
+	
+  private static int winxpos=0,winypos=0;      // place window here
+
+
+  private JButton fishButton, shuffleButton,exitButton, newButton, cheatButton, testButton;
+  private JTextField inputBox;
+  private CardList theDeck = null;
+  private JPanel northPanel;
+  private MyPanel centerPanel;
+  private static JFrame myFrame = null;
+  CardList playerHand = new CardList(0), playerPairs = new CardList(0),
+  		dealerHand = new CardList(0), dealerPairs = new CardList(0);
+  
+
+  ////////////              MAIN      ////////////////////////
+  public static void main(String[] args) {
+     Project3 tpo = new Project3();
+  }
+
+  ////////////            CONSTRUCTOR   /////////////////////
+  public Project3 ()
+  {
+	  
+        myFrame = this;                 // need a static variable reference to a JFrame object
+        northPanel = new JPanel();
+        
+        northPanel.setBackground(Color.white);
+        inputBox = new JTextField("this box will die eventually",20);
+        northPanel.add(inputBox);
+        
+        fishButton = new JButton("Go Fish");
+        northPanel.add(fishButton);
+        fishButton.addActionListener(this);
+        
+        
+        testButton = new JButton("testroonie");
+        northPanel.add(testButton);
+        testButton.addActionListener(this);
+        
+        
+        shuffleButton = new JButton("Shuffle");
+        northPanel.add(shuffleButton);
+        shuffleButton.addActionListener(this);
+        
+        newButton = new JButton("New Deck");
+        northPanel.add(newButton);
+        newButton.addActionListener(this);
+        
+        
+        cheatButton = new JButton("Cheat");
+        northPanel.add(cheatButton);
+        cheatButton.addActionListener(this);
+        
+        
+        exitButton = new JButton("Exit");
+        northPanel.add(exitButton);
+        exitButton.addActionListener(this);
+        
+        
+        getContentPane().add("North",northPanel);
+
+        centerPanel = new MyPanel();
+        getContentPane().add("Center",centerPanel);
+
+        //INITIALIZES THE DECK AND SHUFFLES IT
+        theDeck = new CardList(52);
+        theDeck.shuffle();
+        
+        //DEALS 5 CARDS TO PLAYER HAND
+        for(int i = 0; i<=5; i++) 
+        	playerHand.insertCard(theDeck.deleteCard(i));
+        
+        //DEALS 5 CARDS TO DEALER HAND
+        for(int i = 0; i<=5; i++)
+        	dealerHand.insertCard(theDeck.deleteCard(i));
+        
+        //FLIPS DEALER'S HAND OVER FACE DOWN. CODE REUSE FOR SINGLE INSTANCE OF THE CHEAT BUTTON.
+        Card current = dealerHand.getFirstCard();
+        
+        while(current != null) {
+	        if(current.getIsFaceDown() == false) {
+	        	current.setCardImage(Project3.load_picture("images/gbCard52.gif"));
+	        	current.setIsFaceDown(true);
+	        }
+	        else {
+	        	current.setCardImage(Project3.load_picture("images/gbCard" + current.getCardnum() + ".gif"));
+	        	current.setIsFaceDown(false);
+	        }
+	        current = current.getNextCard();
+        }
+        
+        
+        
+        
+        
+
+        setSize(895,1200);
+        setLocation(winxpos,winypos);
+
+        setVisible(true);
+   }
+  
+//  public void startGame() {
+//	  for(int i = 0; i <= 5; i++)
+//		  deal(theDeck,playerHand);
+//	  
+//  }
+//
+//  public void deal(CardList deck, CardList hand) {
+//	  Card current = deck.getFirstCard();
+//	 hand.insertCard(current);
+//	 deck.getFirstCard() = deck.get
+////	 deck.getFirstCard() = null;
+//	 
+//	  
+//  }
+
+  ////////////   BUTTON CLICKS ///////////////////////////
+  public void actionPerformed(ActionEvent e) {
+
+      if (e.getSource()== exitButton) {
+        dispose(); System.exit(0);
+      }
+      
+      
+      if (e.getSource()== fishButton) {
+        String inputmsg = inputBox.getText();
+        inputBox.setText("I do/don't got no stinkin' " + inputmsg);
+        theDeck = null;
+        repaint();
+      }
+      
+      //For testing
+      if(e.getSource() == testButton) {
+    	  String inputmsg = inputBox.getText();
+    	  inputBox.setText("testaroonie");
+    	  
+    	  repaint();
+      }
+      
+      //Shuffles the deck
+       if (e.getSource()== shuffleButton) {
+        String inputmsg = inputBox.getText();
+        inputBox.setText("Shuffle");
+//        playerHand.shuffle();
+        repaint();
+      }
+       
+       
+       //Flips the first card. Needs to be changed to flip the CPU's hand only. Need to get the layout right first.
+       if (e.getSource()== cheatButton) {
+        inputBox.setText("Turn Cheat ON/OFF (show/hide)");
+        
+        Card current = dealerHand.getFirstCard();
+        
+        while(current != null) {
+	        if(current.getIsFaceDown() == false) {
+	        	current.setCardImage(Project3.load_picture("images/gbCard52.gif"));
+	        	current.setIsFaceDown(true);
+	        }
+	        else {
+	        	current.setCardImage(Project3.load_picture("images/gbCard" + current.getCardnum() + ".gif"));
+	        	current.setIsFaceDown(false);
+	        }
+	        current = current.getNextCard();
+        }
+        
+        //load the card with image 52. check which one it is to determine if you're just flipping it back or not
+        
+        repaint();
+      }
+       if (e.getSource()== newButton) {
+        inputBox.setText("New Deck");
+        theDeck = new CardList(52);
+        repaint();
+      }
+  }
+
+
+// This routine will load an image into memory
+//
+public static Image load_picture(String fname)
+{
+        // Create a MediaTracker to inform us when the image has
+        // been completely loaded.
+        Image image;
+        MediaTracker tracker = new MediaTracker(myFrame);
+
+
+        // getImage() returns immediately.  The image is not
+        // actually loaded until it is first used.  We use a
+        // MediaTracker to make sure the image is loaded
+        // before we try to display it.
+
+        image = myFrame.getToolkit().getImage(fname);
+
+        // Add the image to the MediaTracker so that we can wait
+        // for it.
+        tracker.addImage(image, 0);
+        try { tracker.waitForID(0); }
+        catch ( InterruptedException e) { System.err.println(e); }
+
+        if (tracker.isErrorID(0)) { image=null;}
+        return image;
+}
+// --------------   end of load_picture ---------------------------
+
+class MyPanel extends JPanel {
+
+ ////////////    PAINT   ////////////////////////////////
+  public void paintComponent (Graphics g) {
+    //
+	 
+    int xpos = 105, ypos = 25;
+    g.drawString("This is ALL 52 cards in the Deck!", 25, 15);
+    if (playerHand == null) 
+    	return;
+    Card current = dealerHand.getFirstCard();
+    while (current!=null) {
+       Image tempimage = current.getCardImage();
+       g.drawImage(tempimage, xpos, ypos, this);
+       // note: tempimage member variable must be set BEFORE paint is called
+       xpos += 80;
+       if (xpos > 900) {
+          xpos = 105; ypos += 105;
+       }
+       current = current.getNextCard();
+    }
+    
+    
+    xpos = 105; ypos = 550;
+    current = playerHand.getFirstCard();
+    while(current != null) {
+    	Image tempimage = current.getCardImage();
+        g.drawImage(tempimage, xpos, ypos, this);
+        // note: tempimage member variable must be set BEFORE paint is called
+        xpos += 80;
+        if (xpos > 900) {
+           xpos = 105; ypos += 105;
+        }
+        current = current.getNextCard();
+    	
+    	
+    	
+    	
+    }
+  }
+}
+
+}    // End Of class Project3
+
+
+
